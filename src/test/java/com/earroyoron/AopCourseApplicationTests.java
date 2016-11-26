1d0e57a23253bd8d670567c95e384feec1b3ae85
Ernesto Arroyo <ernesto.arroyo@eurobits.es>
2016-11-26 11:32:48 +0100

    Initial commit

---
+++ src/test/java/com/earroyoron/AopCourseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.earroyoron;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AopCourseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
